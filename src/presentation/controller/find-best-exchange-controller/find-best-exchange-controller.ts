dcf6a7b1d1c16a675ba66227b2a7d7c093d0af8a
Lucas Barbosa <barbosa.lucash@gmail.com>
2022-08-05 18:57:07 -0300

    feat: add controller for find best exchange

---
+++ src/presentation/controller/find-best-exchange-controller/find-best-exchange-controller.ts
@@ -0,0 +1,25 @@
+import { ExchangeService } from "../../../core/interfaces/exchange-service";
+import { FindBestExchange } from "../../../core/usecases/find-best-exchange";
+import { badRequest, ok } from "../../helpers/response";
+import { Controller } from "../../protocols/controller";
+
+type FindBestExchangeRequestData = {
+  currency: string;
+};
+
+export class FindBestExchangeController implements Controller<FindBestExchangeRequestData> {
+  private findBestExchange: FindBestExchange;
+
+  constructor(services: ExchangeService[]) {
+    this.findBestExchange = new FindBestExchange(services);
+  }
+
+  async handle({ currency }: FindBestExchangeRequestData) {
+    try {
+      const response = await this.findBestExchange.execute(currency);
+      return ok(response);
+    } catch (err) {
+      return badRequest(err as Error);
+    }
+  }
+}
---
+++ src/presentation/helpers/response.ts
@@ -0,0 +1,13 @@
+import { HttpResponse } from "../protocols/http";
+
+export const ok = (data: any): HttpResponse => ({
+  body: data,
+  statusCode: 200
+});
+
+export const badRequest = (err: Error): HttpResponse => ({
+  body: {
+    message: err.message
+  },
+  statusCode: 400
+});
